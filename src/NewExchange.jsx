116ad853350645c5aa7764a8ee872dc7ee7253a0
Ghazi Hakim <ghazi.hakim@alenia.io>
2025-04-01 15:49:46 +0200

    add fixed rate to exchange component

--- src/NewExchange.jsx
+++ src/NewExchange.jsx
@@ -4,6 +4,7 @@ function NewExchange({rate}) {
     const [amount, setAmount] = useState('');
     const [converted, setConverted] = useState('');
     const [currency, setCurrency] = useState('EUR');
+    const [fixedRate, setFixedRate] = useState('');
 
     function onChangeAmount(e) {
         const newValue = e.target.value;
@@ -16,18 +17,24 @@ function NewExchange({rate}) {
         setAmount(converted);
     }
 
+    function onChangeFixedRate(e) {
+        const newValue = e.target.value;
+        setFixedRate(newValue);
+    }
+
     useEffect(() => {
+        const rateToApply = fixedRate || rate;
         switch (currency) {
             case 'EUR':
-                setConverted(amount !== '' ? (amount * rate).toFixed(2) : '');
+                setConverted(amount !== '' ? (amount * rateToApply).toFixed(2) : '');
                 break;
             case 'USD':
-                setConverted(amount !== '' ? (amount / rate).toFixed(2) : '');
+                setConverted(amount !== '' ? (amount / rateToApply).toFixed(2) : '');
                 break;
             default:
                 // do nothing
         }
-    }, [rate, amount, currency])
+    }, [rate, amount, currency, fixedRate])
 
     return (
         <div>
@@ -38,6 +45,11 @@ function NewExchange({rate}) {
                     <option>EUR</option>
                     <option>USD</option>
                 </select>
+            </label>
+            <br/>
+            <label>
+                Fixed Rate
+                <input type="number" value={fixedRate} onChange={onChangeFixedRate} />
             </label>
             <p>Converted : {converted}</p>
         </div>
