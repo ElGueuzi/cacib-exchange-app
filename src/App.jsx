3a00fe8bbc2ceadcb5f347c02d179fa6802a322b
Ghazi Hakim <ghazi.hakim@alenia.io>
2025-04-01 15:10:20 +0200

    rate calculation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const initailRate = 1.1;
+  const [rate, setRate] = useState(initailRate);
+
+  // execute once after render, use param empty array []
+  useEffect(() => {
+    // code executed after render
+    const interval = setInterval(() => {
+      const randomChange = (Math.random() * 0.1) - 0.05;
+      setRate((initailRate + randomChange).toFixed(2));
+    }, 3000);
+    // clean up
+    return () => clearInterval(interval);
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Exchange rate : {rate}</h1>
     </>
   )
 }
