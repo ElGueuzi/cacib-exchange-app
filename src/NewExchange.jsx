77561a45c76f6a822277729892281229a641ecb2
Ghazi Hakim <ghazi.hakim@alenia.io>
2025-04-01 16:38:53 +0200

    add history

--- src/NewExchange.jsx
+++ src/NewExchange.jsx
@@ -5,22 +5,70 @@ function NewExchange({rate}) {
     const [converted, setConverted] = useState('');
     const [currency, setCurrency] = useState('EUR');
     const [fixedRate, setFixedRate] = useState('');
+    const [history, setHistory] = useState([]);
 
     function onChangeAmount(e) {
         const newValue = e.target.value;
         setAmount(newValue);
+
+        if (newValue) {
+            addHistory(newValue, calculateConverte(fixedRate, rate, newValue, currency), currency, rate, fixedRate);
+        }
     }
 
     function onChangeCurrency(e) {
         const newValue = e.target.value;
         setCurrency(newValue);
         setAmount(converted);
+
+        addHistory(converted, calculateConverte(fixedRate, rate, converted, newValue), newValue, rate, fixedRate);
     }
 
     function onChangeFixedRate(e) {
         const newValue = e.target.value;
         setFixedRate(newValue);
+
+        if (newValue) {
+            addHistory(amount, calculateConverte(newValue, rate, amount, currency), currency, rate, newValue);
+        }
     }
+
+    function calculateConverte(fixedRateParam, rateParam, amountParam, currencyParam) {
+        let rateToApply;
+        
+        if (fixedRateParam) {
+            const variation = Math.abs(fixedRateParam - rateParam);
+            const threshold = 0.02 * rateParam;
+            if (variation > threshold) {
+                rateToApply = rateParam;
+            } else {
+                rateToApply = fixedRateParam;
+            }
+        } else {
+            rateToApply = rateParam;
+        }
+        
+        switch (currencyParam) {
+            case 'EUR':
+                return amountParam ? (amountParam * rateToApply).toFixed(2) : '';
+            case 'USD':
+                return amountParam ? (amountParam / rateToApply).toFixed(2) : '';
+            default:
+                // do nothing
+                return;
+        }
+    }
+
+    function addHistory(amountParam, convertedParam, currencyParam, rateParam, fixedRateParam) {
+        const convertedCurrency = ['EUR', 'USD'].filter(v => v !== currencyParam).at(0);
+        const newEntry = {
+          rate: rateParam,
+          fixedRate: fixedRateParam,
+          amount: amountParam + ' ' + currencyParam,
+          converted: convertedParam + ' ' + convertedCurrency
+        }
+        setHistory(prev => [newEntry, ...prev].slice(0, 5));
+      }
 
     useEffect(() => {
         let rateToApply;
@@ -55,8 +103,8 @@ function NewExchange({rate}) {
                 Amount
                 <input type="number" value={amount} onChange={onChangeAmount} />
                 <select value={currency} onChange={onChangeCurrency} >
-                    <option value="Euro">EUR</option>
-                    <option value="Dollar">USD</option>
+                    <option value="EUR">Euro</option>
+                    <option value="USD">Dollar</option>
                 </select>
             </label>
             <br/>
@@ -65,6 +113,26 @@ function NewExchange({rate}) {
                 <input type="number" value={fixedRate} onChange={onChangeFixedRate} />
             </label>
             <p>Converted : {converted}</p>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Real Rate</th>
+                        <th>Fixed Rate</th>
+                        <th>Amount</th>
+                        <th>Converted</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {history.map((entry, index) => (
+                    <tr key={index}>
+                        <td>{entry.rate}</td>
+                        <td>{entry.fixedRate}</td>
+                        <td>{entry.amount}</td>
+                        <td>{entry.converted}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
     )
 }
